6f29b1cb1fa62ec83f517b0d07d23c01748d3af4
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2023-10-19 15:21:30 -0400

    Add CupWhacker files and headers to other files

--- include/CupDetector.hpp
+++ include/CupDetector.hpp
@@ -15,20 +15,15 @@
 
 /**
  * @brief Sets base values
- * 
- * @param pinNumber 
- * pin to set (between 0 and 3) (J9-J12)
  */
-void CupDetector_Init(int pinNumber);
+void CupDetector_Init();
 
 /**
  * @brief Check if something has been detected
  * 
- * @param pinNumber 
- * pin to set (between 0 and 3) (J9-J12)
  * @return unsigned short
  * 0 if nothing has been detected
  * else if something has been detected
  */
-unsigned short CupDetector_GetCup(int pinNumber);
+unsigned short CupDetector_GetCup();
 
---
+++ include/CupWhacker.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file CupWhacker.hpp
+ * @author William ()
+ * @brief Header file for Cup whacking
+ * @version 0.1
+ * @date 2023-10-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+//include servo
+#include "Distance_sensor/GP2D12.hpp"
+
+/**
+ * @brief Sets base values
+ */
+void CupWhacker_Init();
+
+/**
+ * @brief Whacks a detected cup
+ */
+void CupWhacker_WhackCup();
