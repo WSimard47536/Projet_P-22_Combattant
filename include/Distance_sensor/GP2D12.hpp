31cd091ee7e9f668f9f226f69c895ef2fdfe9534
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2023-10-19 14:57:14 -0400

    create files for distance sensor

---
+++ include/Distance_sensor/GP2D12.hpp
@@ -0,0 +1,29 @@
+/**
+ * @file GP2D12.hpp
+ * @author William ()
+ * @brief Header file for GP2D12 distance sensor
+ * @version 0.1
+ * @date 2023-10-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+#include "LibRobus.h"
+
+/**
+ * @brief Sets base values
+ * 
+ * @param pinNumber 
+ * pin to set (between 0 and 3) (J9-J12)
+ */
+void GP2D12_init();
+
+/**
+ * @brief Reads the IR sensor at specified pin
+ * 
+ * @param pinNumber 
+ * pin to read (between 0 and 3)
+ */
+uint16_t GP2D12_read(int pinNumber);
---
+++ src/Distance_sensor/GP2D12.cpp
@@ -0,0 +1,24 @@
+#include "Distance_sensor/GP2D12.hpp"
+
+/**
+ * @brief Sets base values
+ * 
+ * @param pinNumber 
+ * pin to set (between 0 and 3) (J9-J12)
+ */
+void GP2D12_init(int pinNumber)
+{
+    //s'il y a des choses à initialiser comme des variables globales pour le capteur, faire ici
+}
+
+/**
+ * @brief Reads the IR sensor at specified pin
+ * 
+ * @param pinNumber 
+ * pin to read (between 0 and 3)
+ */
+uint16_t GP2D12_read(int pinNumber)
+{
+    // pinNumber devrait être un #define et on veut simplement caller la fonction de LibRobus (voir wiki librobus/documentation matérielle)
+    // voir https://github.com/UdeS-GRO/LibRobus/wiki/Documentation-mat%C3%A9rielle#capteur-de-distance-gp2d12-et-gp2y0a21yk0f
+}
