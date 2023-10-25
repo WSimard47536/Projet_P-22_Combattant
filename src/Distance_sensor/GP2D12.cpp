470e4639bb15aa4b3ef308cd1138d6ad97581108
Trevorfkl <trevys.tft@gmail.com>
2023-10-25 13:07:41 -0400

    Update GP2D12.cpp

--- src/Distance_sensor/GP2D12.cpp
+++ src/Distance_sensor/GP2D12.cpp
@@ -39,8 +39,6 @@ unsigned short GP2D12_Read(int pinNumber)
     float rightIRD; 
     leftIRD = IR_Read(1);
     rightIRD = IR_Read(2);
-    printf(leftIRD);
-    printf(rightIRD);
 
     return 0.0f;
     // pinNumber devrait être un #define et on veut simplement caller la fonction de LibRobus (voir wiki librobus/documentation matérielle)
